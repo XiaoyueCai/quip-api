a9602e46e85e13e5384dddb9cc61bb95b68ece56
xiao <645367852@qq.com>
2025-02-04 20:22:01 +0800

    feature: send file attachments

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import gc
 import json
 import threading
 import time
+from urllib.parse import urlencode
 
 from quipclient import QuipClient
 from websocket import WebSocketApp
 from config import config
-from slack import send_message, status_channel
+from slack import send_message, status_channel, generate_attachments
 from logger import logger
 
 HEARTBEAT_INTERVAL = 20
@@ -31,17 +32,40 @@ def is_valid_json(s):
     return True
 
 
+def get_file_url(thread_id: str, file_hash: str, file_name: str) -> str:
+    params = {
+        "name": file_name
+    }
+    base_url = f"https://quip.com/-/blob/{thread_id}/{file_hash}"
+    query_string = urlencode(params, encoding="utf-8")
+    return f"{base_url}?{query_string}"
+
+
 def open_websocket(url):
     def on_message(ws, message):
         if is_valid_json(message):
             receive_data = json.loads(message)
             data_type = receive_data.get("type")
+            thread_data = receive_data.get("thread")
             if data_type == "message":
                 msg_data = receive_data.get("message") or {}
                 author_name = msg_data.get("author_name")
                 text = msg_data.get("text")
-                if author_name and text:
-                    send_message(f"{author_name}: {text}")
+                files = msg_data.get("files")
+                if author_name and (text or files):
+                    attachments = None
+                    if files and thread_data:
+                        thread_id = thread_data.get("id")
+                        file_names = []
+                        file_urls = []
+                        for file in files:
+                            file_name = file.get("name")
+                            file_hash = file.get("hash")
+                            file_names.append(file_name)
+                            file_urls.append(get_file_url(
+                                thread_id=thread_id, file_hash=file_hash, file_name=file_name))
+                        attachments = generate_attachments(file_names=file_names, file_urls=file_urls)
+                    send_message(f"{author_name}: {text or ''}", attachments=attachments)
             if data_type == "alive" or data_type == "heartbeat":
                 logger.debug("message")
                 logger.debug(json.dumps(receive_data, indent=4))
--- slack.py
+++ slack.py
@@ -10,12 +10,13 @@ msg_channel = slack_config.get("channel")
 status_channel = slack_config.get("status_channel")
 
 
-def send_message(msg: str = "", channel: str = msg_channel):
+def send_message(msg: str = "", channel: str = msg_channel, attachments: list = None):
     try:
         # 调用 chat_postMessage API 方法，发送消息到指定频道
         response = client.chat_postMessage(
             channel=f'#{channel}',
             text=msg,
+            attachments=attachments,
         )
         # 确认消息发送成功
         assert response["message"]["text"] == msg
@@ -23,6 +24,18 @@ def send_message(msg: str = "", channel: str = msg_channel):
     except SlackApiError as e:
         # 如果出现错误，打印错误信息
         logger.exception(f"Error sending message: {e.response['error']}")
+
+
+def generate_attachments(file_names: list, file_urls: list) -> list:
+    attachments = []
+    for i in range(len(file_names)):
+        file_name = file_names[i]
+        file_url = file_urls[i]
+        attachments.append({
+            "title": file_name,
+            "title_link": file_url
+        })
+    return attachments
 
 
 if __name__ == '__main__':
